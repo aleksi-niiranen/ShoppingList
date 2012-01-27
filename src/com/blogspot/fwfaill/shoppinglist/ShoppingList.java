d458dbc102b161667be98e6e5709f669fba09353
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2012-01-27 19:11:15 +0200

    added action bar when using Android 4

--- src/com/blogspot/fwfaill/shoppinglist/ShoppingList.java
+++ src/com/blogspot/fwfaill/shoppinglist/ShoppingList.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
@@ -16,83 +17,86 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 public class ShoppingList extends ListActivity {
 	private static final int ACTIVITY_CREATE = 0;
 	private static final int ACTIVITY_EDIT = 1;
-	
+
 	// for menus
-	private static final int INSERT_ID = Menu.FIRST;
+	private static final int INSERT_ID = R.id.item1;
 	private static final int DELETE_ID = Menu.FIRST + 1;
-	
+
 	private ShoppingListDbAdapter mDbHelper;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        mDbHelper = new ShoppingListDbAdapter(this);
-        mDbHelper.open();
-        fillData();
-        registerForContextMenu(getListView());
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mDbHelper = new ShoppingListDbAdapter(this);
+		mDbHelper.open();
+		fillData();
+		registerForContextMenu(getListView());
+	}
 
 	private void fillData() {
 		Cursor shoppingListsCursor = mDbHelper.fetchAllShoppingLists();
 		startManagingCursor(shoppingListsCursor);
-		
+
 		// Create an array to specify the fields we want to display in the list
-		String[] from = new String[] {ShoppingListDbAdapter.KEY_TITLE};
-		
+		String[] from = new String[] { ShoppingListDbAdapter.KEY_TITLE };
+
 		// and an array of the fields we want to bind those fields to
-		int[] to = new int[]{R.id.text1};
-		
+		int[] to = new int[] { R.id.text1 };
+
 		// Now create a simple cursor adapter and set it to display
-		SimpleCursorAdapter shoppingLists =
-				new SimpleCursorAdapter(this, R.layout.shoppinglistrow, shoppingListsCursor, from, to);
+		SimpleCursorAdapter shoppingLists = new SimpleCursorAdapter(this,
+				R.layout.shoppinglistrow, shoppingListsCursor, from, to);
 		setListAdapter(shoppingLists);
 	}
 
 	/**
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-    	menu.add(0, INSERT_ID, 0, R.string.list_menu_add_list);
-    	return true;
-    }
-    
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-    	switch (item.getItemId()) {
-    		case INSERT_ID:
-    			createList();
-    			return true;
-    	}
-    	return super.onMenuItemSelected(featureId, item);
-    }
-    
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-    	super.onCreateContextMenu(menu, v, menuInfo);
-    	menu.add(0, DELETE_ID, 0, R.string.remove);
-    }
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case INSERT_ID:
+			createList();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.remove);
+	}
 
 	private void createList() {
 		Intent i = new Intent(this, EditList.class);
 		startActivityForResult(i, ACTIVITY_CREATE);
 	}
-	
+
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case DELETE_ID:
-				AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-				mDbHelper.deleteShoppingList(info.id);
-				fillData();
-				return true;
+		case DELETE_ID:
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+					.getMenuInfo();
+			mDbHelper.deleteShoppingList(info.id);
+			fillData();
+			return true;
 		}
 		return super.onContextItemSelected(item);
 	}
-	
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
@@ -100,13 +104,14 @@ public class ShoppingList extends ListActivity {
 		i.putExtra(ShoppingListDbAdapter.KEY_ROWID, id);
 		startActivityForResult(i, ACTIVITY_EDIT);
 	}
-	
+
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 		fillData();
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
